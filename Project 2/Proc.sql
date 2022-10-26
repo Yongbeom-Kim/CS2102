b1cd9d1887c6552257524bbf3f5cf39ba415702a
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-10-26 20:46:50 +0800

    Fix checkstyle

--- Project 2/Proc.sql
+++ Project 2/Proc.sql
@@ -33,90 +33,81 @@ CREATE OR REPLACE TRIGGER back_project
 BEFORE INSERT OR UPDATE ON Backs 
 FOR EACH ROW EXECUTE FUNCTION check_back();
 
-/* ------------------------ */
 /* ----- PROECEDURES  ----- */
 /* Procedure #1 */
-CREATE
-OR REPLACE PROCEDURE add_user(
-  email TEXT,
-  name TEXT,
-  cc1 TEXT,
-  cc2 TEXT,
-  street TEXT,
-  num TEXT,
-  zip TEXT,
-  country TEXT,
-  kind TEXT
-) AS $ $ -- add declaration here
-BEGIN -- your code here
+CREATE OR REPLACE PROCEDURE add_user(
+  email TEXT, name    TEXT, cc1  TEXT,
+  cc2   TEXT, street  TEXT, num  TEXT,
+  zip   TEXT, country TEXT, kind TEXT
+) AS $$
+-- add declaration here
+BEGIN
+  -- your code here
 END;
+$$ LANGUAGE plpgsql;
 
-$ $ LANGUAGE plpgsql;
+
 
 /* Procedure #2 */
-CREATE
-OR REPLACE PROCEDURE add_project(
-  id INT,
-  email TEXT,
-  ptype TEXT,
-  created DATE,
-  name TEXT,
-  deadline DATE,
-  goal NUMERIC,
-  names TEXT [],
-  amounts NUMERIC []
-) AS $ $ -- add declaration here
-BEGIN -- your code here
+CREATE OR REPLACE PROCEDURE add_project(
+  id      INT,     email TEXT,   ptype    TEXT,
+  created DATE,    name  TEXT,   deadline DATE,
+  goal    NUMERIC, names TEXT[],
+  amounts NUMERIC[]
+) AS $$
+-- add declaration here
+BEGIN
+  -- your code here
 END;
+$$ LANGUAGE plpgsql;
 
-$ $ LANGUAGE plpgsql;
+
 
 /* Procedure #3 */
-CREATE
-OR REPLACE PROCEDURE auto_reject(eid INT, today DATE) AS $ $ -- add declaration here
-BEGIN -- your code here
+CREATE OR REPLACE PROCEDURE auto_reject(
+  eid INT, today DATE
+) AS $$
+-- add declaration here
+BEGIN
+  -- your code here
 END;
-
-$ $ LANGUAGE plpgsql;
-
+$$ LANGUAGE plpgsql;
 /* ------------------------ */
+
+
+
+
+
 /* ----- FUNCTIONS    ----- */
 /* Function #1  */
-CREATE
-OR REPLACE FUNCTION find_superbackers(today DATE) RETURNS TABLE(email TEXT, name TEXT) AS $ $ -- add declaration here
-BEGIN -- your code here
+CREATE OR REPLACE FUNCTION find_superbackers(
+  today DATE
+) RETURNS TABLE(email TEXT, name TEXT) AS $$
+-- add declaration here
+BEGIN
+  -- your code here
 END;
+$$ LANGUAGE plpgsql;
 
-$ $ LANGUAGE plpgsql;
+
 
 /* Function #2  */
-CREATE
-OR REPLACE FUNCTION find_top_success(n INT, today DATE, ptype TEXT) RETURNS TABLE(
-  id INT,
-  name TEXT,
-  email TEXT,
-  amount NUMERIC
-) AS $ $
-SELECT
-  1,
-  '',
-  '',
-  0.0;
+CREATE OR REPLACE FUNCTION find_top_success(
+  n INT, today DATE, ptype TEXT
+) RETURNS TABLE(id INT, name TEXT, email TEXT,
+                amount NUMERIC) AS $$
+  SELECT 1, '', '', 0.0; -- replace this
+$$ LANGUAGE sql;
 
--- replace this
-$ $ LANGUAGE sql;
+
 
 /* Function #3  */
-CREATE
-OR REPLACE FUNCTION find_top_popular(n INT, today DATE, ptype TEXT) RETURNS TABLE(
-  id INT,
-  name TEXT,
-  email TEXT,
-  days INT
-) AS $ $ -- add declaration here
-BEGIN -- your code here
+CREATE OR REPLACE FUNCTION find_top_popular(
+  n INT, today DATE, ptype TEXT
+) RETURNS TABLE(id INT, name TEXT, email TEXT,
+                days INT) AS $$
+-- add declaration here
+BEGIN
+  -- your code here
 END;
-
-$ $ LANGUAGE plpgsql;
-
-/* ------------------------ */
+$$ LANGUAGE plpgsql;
