d4ead449a96ad4949599e5ec1374938466df712c
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-10-31 20:29:55 +0800

    Solution for Function 3

--- Project 2/Proc.sql
+++ Project 2/Proc.sql
@@ -152,22 +152,53 @@ $$ LANGUAGE plpgsql;
 
 
 /* Function #2  */
+/*Write a function to find project id, the name of the project, the email of
+the creator, and the amount pledged for the project for the top N most
+successful project based on the success metric for given date (i.e., the
+project deadline must be before the given date) and for the given project
+type*/
+
 CREATE OR REPLACE FUNCTION find_top_success(
   n INT, today DATE, ptype TEXT
 ) RETURNS TABLE(id INT, name TEXT, email TEXT,
                 amount NUMERIC) AS $$
-  SELECT 1, '', '', 0.0; -- replace this
+  SELECT id, name, email, (SELECT SUM(amount) FROM Backs b WHERE b.id = p.id) as amount
+  FROM Projects p
+  WHERE p.deadline < today AND p.ptype = ptype
+  ORDER BY (((SELECT SUM(amount) FROM Backs b WHERE b.id = p.id) + (0.00))/p.goal) DESC,
+    p.deadline DESC,
+    p.id ASC
+  LIMIT n;
+
 $$ LANGUAGE sql;
 
 
 
 /* Function #3  */
+/*3. Write a function to find the project id, name of the project, the email of
+the creator, and the number of days it takes for the project to reach its
+funding goal for the top N most popular project based on the popularity
+metric for the given date (i.e., the project must be created before today)
+and for the given project type.*/
 CREATE OR REPLACE FUNCTION find_top_popular(
   n INT, today DATE, ptype TEXT
 ) RETURNS TABLE(id INT, name TEXT, email TEXT,
                 days INT) AS $$
+DECLARE
+  
 -- add declaration here
 BEGIN
-  -- your code here
+  RETURN QUERY
+  SELECT p1.id, (MIN(b1.request) - p1.created) as days
+  FROM Backs b1 
+      JOIN Backs b2 ON b2.id = b1.id AND b2.request <= b1.request
+      JOIN Projects p1 on p1.id = b1.id
+  GROUP BY b1.request, p1.id, p1.goal, p1.id, p1.created
+  HAVING SUM(b2.amount) >= p1.goal
+      AND p1.ptype = ptype
+      AND p1.created < today
+  ORDER BY (MIN(b1.request) - p1.created) ASC,
+      p1.id ASC
+  LIMIT n;
 END;
 $$ LANGUAGE plpgsql;
