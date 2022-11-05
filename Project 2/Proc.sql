eddca058a25b7c28d2a3c7eb09c63851f1539d97
Pei Cheng Yi <53963433+SeekSaveServe@users.noreply.github.com>
2022-11-05 16:42:54 +0800

    Fix aliasing issue

--- Project 2/Proc.sql
+++ Project 2/Proc.sql
@@ -63,34 +63,45 @@ FOR EACH ROW EXECUTE FUNCTION check_back();
 
 /* ----- PROECEDURES  ----- */
 /* Procedure #1 */
+DROP PROCEDURE add_new_user(text, text, text, text);
+DROP PROCEDURE add_user(text,text,text,text,text,text,text,text,text);
+
+CREATE OR REPLACE PROCEDURE add_new_user(
+email_ TEXT, name TEXT, cc1 TEXT, cc2 TEXT
+) AS $$
+BEGIN
+  /* check user */
+  IF (email_ is not null AND name is not null AND cc1 is not null) THEN
+  -- add if user does not exist in table 
+    IF (NOT EXISTS(SELECT * FROM Users as u WHERE u.email = email_) = true) THEN
+      INSERT INTO Users VALUES(email_, name, cc1, cc2);
+    END IF;
+  END IF;
+END
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE PROCEDURE add_user(
-  email TEXT, name    TEXT, cc1  TEXT,
+  email_ TEXT, name    TEXT, cc1  TEXT,
   cc2   TEXT, street  TEXT, num  TEXT,
   zip   TEXT, country TEXT, kind TEXT
 ) AS $$
 BEGIN
-  /* check user */
-  IF (email is not null AND name is not null AND cc1 is not null) THEN
-  -- add if user does not exist in table 
-    IF (NOT EXISTS(SELECT email FROM Users as u WHERE u.email = email) = true) THEN
-      INSERT INTO Users(email, name, cc1, cc2) VALUES (email, name, cc1, cc2);
-    END IF;
-  END IF;
+  CALL add_new_user(email_, name, cc1, cc2);
   /* the following can only be done if the user EXISTS */
-  IF (EXISTS(SELECT * FROM Users as u WHERE u.email = email)) THEN
+  IF (EXISTS(SELECT * FROM Users as u WHERE u.email = email_)) THEN
     /* if Creator, check creator and then add */
     IF (kind = 'BACKER' or kind = 'BOTH') THEN
       IF (street is not null AND num is not null AND zip is not null AND country is not null) THEN
-        IF (NOT EXISTS(SELECT * FROM Backers as b WHERE b.email = email)) THEN
-          INSERT INTO Backers(email, street, num, zip, country) VALUES(email, street, num, zip, country);
+        IF (NOT EXISTS(SELECT * FROM Backers as b WHERE b.email = email_)) THEN
+          INSERT INTO Backers VALUES(email_, street, num, zip, country);
       END IF;
     END IF;
 	END IF;
     /* if backer , check backer and then add */
     IF (kind = 'CREATOR' or kind = 'BOTH') THEN
       IF (country is not null) THEN
-        IF (NOT EXISTS(SELECT * FROM Creators as c WHERE c.email = email)) THEN
-          INSERT INTO Creators(email,country) VALUES(email, country);
+        IF (NOT EXISTS(SELECT * FROM Creators as c WHERE c.email = email_)) THEN
+          INSERT INTO Creators VALUES(email_, country);
       END IF;
     END IF;
   END IF;
